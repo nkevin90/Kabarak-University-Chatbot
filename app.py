85fa2ce54bdea5359e29191d60a9a620c9189e05
Kelvin Njenga <njengakelvin@kabarak.ac.ke>
2023-03-02 14:21:39 +0300

    Up and Running(bug fixed)

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 from flask import Flask, render_template, request, jsonify
-
-
 from chat import get_response
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-## Flask has no attribute get or post
-
-@app.route("/", methods=[GET])
+@app.route("/", methods=["GET"])
 def index_get():
     return render_template("base.html")
 
-
-@app.route("/predict", methods=[GET])
+@app.route("/predict", methods=["POST"])
 def predict():
-    text= request.get_json().get("message")
-    response=get_response(text)
-    message={"answer":response}
-    return jsonify(message)
+    message = request.json["message"]
+    response = get_response(message)
+    return jsonify({"answer": response})
 
 if __name__ == "__main__":
     app.run(debug=True)
